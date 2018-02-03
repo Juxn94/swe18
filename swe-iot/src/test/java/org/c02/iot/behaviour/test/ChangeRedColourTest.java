92422fc8af15031d85a54d16c9ed84571bbf402a
FlorianWinkler <florian.winkler@edu.campus02.at>
2018-02-03 12:17:44 +0100

    ChangeRecColour

---
+++ swe-iot/src/test/java/org/c02/iot/behaviour/test/ChangeRedColourTest.java
@@ -0,0 +1,33 @@
+package org.c02.iot.behaviour.test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.awt.Color;
+
+import org.c02.swe.iot.Button;
+import org.c02.swe.iot.IButton.ButtonDirection;
+import org.c02.swe.iot.behaviour.ChangeRedColour;
+import org.c02.swe.iot.cloud.api.IParticleApi;
+import org.c02.swe.iot.cloud.api.ParticleException;
+import org.junit.Test;
+
+public class ChangeRedColourTest {
+	
+	@Test
+	public void testIncreaseRed() throws ParticleException {
+		/*
+		Button b = mock(Button.class);
+		ChangeRedColour crc = new ChangeRedColour(b);
+		
+		when(b.getButtonClickCounter(ButtonDirection.East)).thenReturn(2);
+		
+
+		
+		verify(b).getButtonClickCounter(ButtonDirection.East);
+		verify(b).setLed(1, new Color(235,255,255));
+		*/
+		
+	}
+
+}
